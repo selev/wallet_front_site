efe0b1e73bfd15b0301e644d7652eaf5abcdf852
Raul <rap_dnmv@hotmail.com>
2023-04-24 21:29:41 -0500

    Cambios iniciales para el desarrollo de fondo(Back end)

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 
-var nombre;
+var nombreF;
+var salarioF;
+var gastoF;
+var formulario;
+
+
+
 
 function imprimir(){
-   nombre= document.getElementById('EntradaNombre').value;
-    if(nombre==null){
-        console.log("no hay nada");
-    }else{
-        console.log(nombre);
+   
+
+    event.preventDefault();
+    TomarDatos();  
+    enviarFormulario();  
+}
+
+function TomarDatos(){
+    
+    nombreF= document.getElementById('EntradaNombre').value;
+    salarioF= document.getElementById('EntradaSalario').value;
+    gastoF= document.getElementById('EntradaGastos').value;
+    formulario={
+        nombre:nombreF,
+        salario:salarioF,
+        gastos:gastoF
     }
 
-    event.preventDefault();    
 }
+
+
+
+function enviarFormulario(){
+    console.log("entro al fetch");
+    fetch("https://2ac22411-09e1-451f-a8d7-bf9b048b892c.mock.pstmn.io/wallet/finanzas",
+    {method: 'post',
+    body:JSON.stringify(formulario)}
+    )
+    ;
+
+}
+
+//fetch("")
